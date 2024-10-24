18e027ba85cab657c360a42ecd8712fbf7378164
Sir Atops <aetops93@proton.me>
2024-10-24 15:42:03 +1030

    changes

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,1 +1,1 @@
-yay -S cliphist wl-clipboard hyprland waybar swww alacritty wofi neovim pipewire pipewire-pulse
+yay -S ttf-jetbrains-mono-nerd cliphist wl-clipboard hyprland waybar swww alacritty wofi neovim pipewire pipewire-pulse wireplumber lib32-pipewire pipewire-alsa vim
