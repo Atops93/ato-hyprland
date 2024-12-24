22061d2099aca09bfc8c9c09548a11c1996f4f95
Sir Atops <aetops93@proton.me>
2024-12-25 00:20:58 +1030

    dwad

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,1 +1,1 @@
-yay -S ttf-jetbrains-mono-nerd cliphist wl-clipboard hyprland waybar swww alacritty wofi neovim pipewire pipewire-pulse wireplumber lib32-pipewire pipewire-alsa vim
+yay -S ttf-jetbrains-mono-nerd cliphist wl-clipboard hyprland waybar swww wofi pipewire pipewire-pulse wireplumber lib32-pipewire pipewire-alsa
