ad337c6957b50658edeae8d7a93c7c2255901df0
Sir Atops <aetops93@proton.me>
2025-01-08 23:08:13 +1030

    change border size & added sof-firmware to fix my audio on my machine

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,1 +1,1 @@
-yay -S ttf-jetbrains-mono-nerd cliphist wl-clipboard hyprland waybar swww wofi pipewire pipewire-pulse wireplumber lib32-pipewire pipewire-alsa
+yay -S ttf-jetbrains-mono-nerd cliphist wl-clipboard hyprland waybar swww wofi pipewire pipewire-pulse sof-firmware
